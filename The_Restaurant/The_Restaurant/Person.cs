37a42ededeaec13aab6eb0f1933db4f225374906
Ziriado <115992429+Ziriado@users.noreply.github.com>
2022-10-27 10:59:16 +0200

    Added Table

--- The_Restaurant/The_Restaurant/Person.cs
+++ The_Restaurant/The_Restaurant/Person.cs
@@ -36,5 +36,6 @@ namespace The_Restaurant
         {
 
         }
+     
     }
 }
---
+++ The_Restaurant/The_Restaurant/Table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Restaurant
+{
+    internal class Table
+    {
+        public bool isOccupied { get; set; }
+
+        public bool isDirty { get; set; }
+
+        public int tableSize { get; set; }
+
+        public Table(bool isOccupied,bool isDirty,int tableSize)
+        {
+            
+        }
+    }
+
+    
+}
