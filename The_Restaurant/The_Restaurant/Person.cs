805400c35b0e2de380c36bccdda2737fbf8abe71
Ziriado <115992429+Ziriado@users.noreply.github.com>
2022-10-27 10:00:25 +0200

    Added some classes

---
+++ The_Restaurant/The_Restaurant/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Restaurant
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+    class Chef :Person
+    {
+        public Chef(string Name) : base(Name)
+        {
+
+        }
+    }
+    class Waiter : Person
+    {
+        public Waiter(string Name) : base(Name)
+        {
+
+        }
+    }
+
+    class Guest : Person
+    {
+        public Guest (string Name) :base(Name)
+        {
+
+        }
+    }
+}
