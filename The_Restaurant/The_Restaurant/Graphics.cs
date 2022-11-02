735b26f67e3fe0f91e1a1bdb1a88a13863f9acdb
Douglas <doggek@hotmail.se>
2022-11-02 13:10:03 +0100

    Changed  _tableList to TableDictionary

--- The_Restaurant/The_Restaurant/Graphics.cs
+++ The_Restaurant/The_Restaurant/Graphics.cs
@@ -9,35 +9,36 @@ namespace The_Restaurant
     internal class Graphics
     {
         public List<Table> _tableList = new List<Table>();
+        public Dictionary<string, Table> tableDictionary = new Dictionary<string, Table>();
 
         public void DrawRestaurant(List <Guest> guests)
         {
             List<Guest> empty = new List<Guest>();
             
-            _tableList.Add(new Table { Name = "Bord 1", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 4, SetX = 0, SetY = 7 });
-            _tableList.Add(new Table { Name = "Bord 2", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 4, SetX = 20, SetY = 7 });
-            _tableList.Add(new Table { Name = "Bord 3", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 4, SetX = 40, SetY = 7 });
-            _tableList.Add(new Table { Name = "Bord 4", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 4, SetX = 60, SetY = 7 });
-            _tableList.Add(new Table { Name = "Bord 5", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 4, SetX = 80, SetY = 7 });
-            _tableList.Add(new Table { Name = "Bord 6", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 2, SetX = 0, SetY = 17 });
-            _tableList.Add(new Table { Name = "Bord 7", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 2, SetX = 20, SetY = 17 });
-            _tableList.Add(new Table { Name = "Bord 8", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 2, SetX = 40, SetY = 17 });
-            _tableList.Add(new Table { Name = "Bord 9", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 2, SetX = 60, SetY = 17 });
-            _tableList.Add(new Table { Name = "Bord 10", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 2, SetX = 80, SetY = 17 });
+            tableDictionary.Add("Bord 1", new Table { Name = "Bord 1", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 4, SetX = 0, SetY = 7 });
+            tableDictionary.Add("Bord 2", new Table { Name = "Bord 2", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 4, SetX = 20, SetY = 7 });
+            tableDictionary.Add("Bord 3", new Table { Name = "Bord 3", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 4, SetX = 40, SetY = 7 });
+            tableDictionary.Add("Bord 4", new Table { Name = "Bord 4", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 4, SetX = 60, SetY = 7 });
+            tableDictionary.Add("Bord 5", new Table { Name = "Bord 5", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 4, SetX = 80, SetY = 7 });
+            tableDictionary.Add("Bord 6", new Table { Name = "Bord 6", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 2, SetX = 0, SetY = 17 });
+            tableDictionary.Add("Bord 7", new Table { Name = "Bord 7", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 2, SetX = 20, SetY = 17 });
+            tableDictionary.Add("Bord 8", new Table { Name = "Bord 8", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 2, SetX = 40, SetY = 17 });
+            tableDictionary.Add("Bord 9", new Table { Name = "Bord 9", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 2, SetX = 60, SetY = 17 });
+            tableDictionary.Add("Bord 10", new Table { Name = "Bord 10", IsDirty = false, IsOccupied = false, TableQuality = 5, TableSize = 2, SetX = 80, SetY = 17 });
 
             Draw("Kö", 98, 0, empty);
             Draw("Köket", 40, 0, empty);
             Draw("Diskbås", 53, 0, empty);
 
-            foreach (Table table in _tableList)
+            foreach (KeyValuePair<string, Table> table in tableDictionary)
             {
-                if (table.IsDirty == false && table.IsOccupied == false && guests is not null)
+                if (table.Value.IsDirty == false && table.Value.IsOccupied == false && guests is not null)
                 {
-                    Draw(table.Name, table.SetX, table.SetY, guests);
+                    Draw(table.Value.Name, table.Value.SetX, table.Value.SetY, guests);
                 }
                 else
                 {
-                    Draw(table.Name, table.SetX, table.SetY, empty);
+                    Draw(table.Value.Name, table.Value.SetX, table.Value.SetY, empty);
                 }
                 guests.Clear();
             }
