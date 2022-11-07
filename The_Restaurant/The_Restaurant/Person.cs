48df7b18049625f329ed4840a5ea89b033b7c108
Ziriado <115992429+Ziriado@users.noreply.github.com>
2022-11-07 10:49:35 +0100

    Everything was not added

--- The_Restaurant/The_Restaurant/Person.cs
+++ The_Restaurant/The_Restaurant/Person.cs
@@ -28,22 +28,27 @@ namespace The_Restaurant
         public int SetY { get; set; }
         public bool IsAvailable { get; set; }
         public string Name { get; set; }
-
+        public int Skill { get; set; }
         public Waitress(bool isAvailable, int setX, int setY)
         {
             Name = The_Restaurant.Company.LastName();
             SetX = setX;
             SetY = setY;
             IsAvailable = isAvailable;
+            Random rnd = new Random();
+            Skill = rnd.Next(1, 6);
         }
     }
     class Chef : IPerson
     {
         public string Name { get; set; }
+        public int Skill { get; set; }
 
         public Chef()
         {
             Name = The_Restaurant.Company.LastName();
+            Random rnd = new Random();
+            Skill = rnd.Next(1, 6);
         }
     }
 }
