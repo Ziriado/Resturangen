2f37ca922e9ca39527513a7a2d71dbf372c8f624
Ziriado <115992429+Ziriado@users.noreply.github.com>
2022-11-07 10:48:39 +0100

    Added several calc method and call methods

--- The_Restaurant/The_Restaurant/Menu.cs
+++ The_Restaurant/The_Restaurant/Menu.cs
@@ -7,100 +7,82 @@ using System.Threading.Tasks;
 namespace The_Restaurant
 {
 
-    //    public class Menu
-    //    {
-    //        public double Price { get; set; }
-    //        public string Name { get; set; }
-    //        public Menu(double price, string name)
-    //        {
-    //            Price = price;
-    //            Name = name;
-    //        }
-    //    }
-
-    //    public class Flounder : Menu
-    //    {
-    //        public Flounder(double price, string name) : base(price, name)
-    //        {
-
-
-    //        }
-    //    }
-    //    public class Salmon : Menu
-    //    {
-    //        public Salmon(double price, string name) : base(price, name)
-    //        {
-
-    //        }
-    //    }
-    //    public class Cod : Menu
-    //    {
-    //        public Cod(double price, string name) : base(price, name)
-    //        {
-
-    //        }
-    //    }
-    //    public class Sirloin_Steak : Menu
-    //    {
-    //        public Sirloin_Steak(double price, string name) : base(price, name)
-    //        {
-
-    //        }
-    //    }
-    //    public class Bolognese : Menu
-    //    {
-    //        public Bolognese(double price, string name) : base(price, name)
-    //        {
-
-    //        }
-    //    }
-    //    public class Beef_Tender_Loin : Menu
-    //    {
-    //        public Beef_Tender_Loin(double price, string name) : base(price, name)
-    //        {
-
-    //        }
-    //    }
-    //    public class Halloumi : Menu
-    //    {
-    //        public Halloumi(double price, string name) : base(price, name)
-    //        {
-
-    //        }
-    //    }
-    //    public class Cauliflower_Mash : Menu
-    //    {
-    //        public Cauliflower_Mash(double price, string name) : base(price, name)
-    //        {
-
-    //        }
-    //    }
-    //    public class Vego_HappyMeal : Menu
-    //    {
-    //        public Vego_HappyMeal(double price, string name) : base(price, name)
-    //        {
-
-    //        }
-
-    //    }
-    //}
-    internal class Menu
+    public class Menu
     {
-        public int Price { get; set; }
-
-        public List<Menu> Dishes { get; set; }
-
-        public Menu(int price)
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public Menu(double price, string name)
         {
             Price = price;
-            Dishes = new List<Menu>();
+            Name = name;
+        }
+    }
+
+    public class Flounder : Menu
+    {
+        public Flounder(double price, string name) : base(price, name)
+        {
 
 
         }
-        public void getDishes()
+    }
+    public class Salmon : Menu
+    {
+        public Salmon(double price, string name) : base(price, name)
         {
 
         }
     }
+    public class Cod : Menu
+    {
+        public Cod(double price, string name) : base(price, name)
+        {
+
+        }
+    }
+    public class Sirloin_Steak : Menu
+    {
+        public Sirloin_Steak(double price, string name) : base(price, name)
+        {
+
+        }
+    }
+    public class Bolognese : Menu
+    {
+        public Bolognese(double price, string name) : base(price, name)
+        {
+
+        }
+    }
+    public class Beef_Tender_Loin : Menu
+    {
+        public Beef_Tender_Loin(double price, string name) : base(price, name)
+        {
+
+        }
+    }
+    public class Halloumi : Menu
+    {
+        public Halloumi(double price, string name) : base(price, name)
+        {
+
+        }
+    }
+    public class Cauliflower_Mash : Menu
+    {
+        public Cauliflower_Mash(double price, string name) : base(price, name)
+        {
+
+        }
+    }
+    public class Vego_HappyMeal : Menu
+    {
+        public Vego_HappyMeal(double price, string name) : base(price, name)
+        {
+
+        }
+
+    }
 }
+
 
